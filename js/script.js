fcc9fb7e8712f52affb226bb96c8c5dcb1cf1b3c
guruxkancharla <guruvardhan1001@gmail.com>
2022-03-19 20:46:46 +0530

    script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-console.log("Hello world!");
-
-const myName = "Jonas Schmedtmann";
 const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
@@ -15,29 +12,6 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
-
-// const allLinks = document.querySelectorAll("a:link");
-
-// allLinks.forEach(function (link) {
-//   link.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     const href = link.getAttribute("href");
-
-//     if (href === "#")
-//       window.scrollTo({
-//         top: 0,
-//         behavior: "smooth",
-//       });
-
-//     if (href !== "#" && href.startsWith("#")) {
-//       const sectionEl = document.querySelector(href);
-//       sectionEl.scrollIntoView({ behavior: "smooth" });
-//     }
-
-//     if (link.classList.contains("main-nav-link"))
-//       headerEl.classList.toggle("nav-open");
-//   });
-// });
 
 const sectionHeroEl = document.querySelector(".section-hero");
 
